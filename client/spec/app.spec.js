68d47d4b1d341e146ce12f985ff51ea56583d335
Jim Cowart <jim@ifandelse.com>
2015-11-15 23:57:59 -0600

    Updated drone file to use browser image, added app.spec.js

---
+++ client/spec/app.spec.js
@@ -0,0 +1,48 @@
+import appFactory from "inject!app";
+
+describe( "app", () => {
+	let promiseStub, errorHandlerStub;
+
+	beforeEach( () => {
+		errorHandlerStub = {
+			handleRejection: _.noop,
+			handleException: _.noop
+		};
+
+		promiseStub = {};
+
+		// Initialize factory
+		appFactory( {
+			react: {},
+			jquery: {},
+			when: {
+				Promise: promiseStub
+			},
+			"infrastructure/errorHandler": errorHandlerStub,
+			"infrastructure/api": {},
+			"infrastructure/router": {}
+		} );
+	} );
+
+	describe( "logging unhandled promise rejections", () => {
+		it( "should add a handler to when's onPotentiallyUnhandledRejection property", () => {
+			promiseStub.onPotentiallyUnhandledRejection.should.equal( errorHandlerStub.handleRejection );
+		} );
+	} );
+
+	describe( "logging unhandled exceptions", () => {
+		let existingOnError;
+
+		before( () => {
+			existingOnError = window.onerror;
+		} );
+
+		after( () => {
+			window.onerror = existingOnError;
+		} );
+
+		it( "should attach an onerror handler to the window", () => {
+			window.onerror.should.equal( errorHandlerStub.handleException );
+		} );
+	} );
+} );
