74dd5594fa88c0f892bfd11942b0eafe9630a131
Elijah Manor <elijah.manor@gmail.com>
2015-11-19 14:58:59 -0600

    Add helper data file

---
+++ client/spec/data/hostWithStatus.js
@@ -0,0 +1,13 @@
+module.exports = {
+	name: "name",
+	ip: "ip",
+	projectName: "projectName",
+	owner: "owner",
+	hostName: "hostName",
+	status: {
+		hostUptime: "hostUptime",
+		serviceUptime: "serviceUptime",
+		slug: "slug",
+		version: "version"
+	}
+};
