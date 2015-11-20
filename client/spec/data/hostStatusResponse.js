f01d1eff66111aa5cb60539fdcdd524ade48df46
Matt Dunlap <matt.dunlap@leankit.com>
2015-11-20 10:16:21 -0700

    Api tests and minor fix
    - API test coverage should be at 100% now
    - Handling applySettings response and firing success/error actions

---
+++ client/spec/data/hostStatusResponse.js
@@ -0,0 +1,49 @@
+module.exports = {
+	uptime: {
+		host: "17 hours 17 minutes 48 seconds",
+		service: "17 hours 4 minutes 55 seconds"
+	},
+	"package": {
+		project: "littlebrudder",
+		owner: "arobson",
+		branch: "master",
+		version: "0.1.0"
+	},
+	activity: {
+		running: {
+			branch: "master",
+			owner: "arobson",
+			project: "/usr/src/app/downloads/littlebrudder",
+			slug: "1e000e7d",
+			version: "0.1.0-7"
+		}
+	},
+	lastEvent: {
+		started: {}
+	},
+	lastFailure: "N/A",
+	time: {
+		sinceLastActivity: "17 hours 4 minutes 55 seconds",
+		sinceLastEvent: "17 hours 4 minutes 55 seconds"
+	},
+	_links: {
+		environment: {
+			href: "/api/status/environment",
+			method: "GET"
+		},
+		self: {
+			href: "/api/status/",
+			method: "GET"
+		},
+		settings: {
+			href: "/api/status/settings",
+			method: "GET"
+		}
+	},
+	_origin: {
+		href: "/api/status",
+		method: "GET"
+	},
+	_resource: "status",
+	_action: "self"
+};
